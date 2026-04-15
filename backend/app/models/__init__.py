d1b6526f756a6995d56dbd51738d85e7c80dae61
hemajoba1414-cpu <hemajoba1414@gmail.com>
2026-04-15 20:52:22 +0200

    Delete backend/app directory

--- backend/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Models package
-from app.models.models import User, Plan, Subscription, Payment, PlanStatus, PlanType
-
-__all__ = ["User", "Plan", "Subscription", "Payment", "PlanStatus", "PlanType"]
--- backend/app/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-# Routes package
-from app.routes.auth import router as auth_router
-from app.routes.plans import router as plans_router
-from app.routes.subscriptions import router as subscriptions_router
-from app.routes.export import router as export_router
-
-__all__ = ["auth_router", "plans_router", "subscriptions_router", "export_router"]
